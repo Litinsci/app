388258b9e7c4f779fe24661711a381a8300b8235
Litinsci <litovchenko.73@list.ru>
2020-09-21 21:41:57 +0300

    litle rework chat

--- static/js/function_new.js
+++ static/js/function_new.js
@@ -1,8 +1,9 @@
 ;
+
 function showHide() {
     if (arguments[0].style.display == "none") {
         for (let element of arguments) {
-        element.style.display = "block";
+            element.style.display = "block";
         }
     } else {
         for (let element of arguments) {
@@ -10,12 +11,12 @@ function showHide() {
         }
     }
 }
-  
-  
+
+
 function showHide_flex() {
     if (arguments[0].style.display == "none") {
         for (let element of arguments) {
-        element.style.display = "flex";
+            element.style.display = "flex";
         }
     } else {
         for (let element of arguments) {
@@ -23,34 +24,36 @@ function showHide_flex() {
         }
     }
 }
-  
-  
-const togle_animate = function(){
+
+
+const togle_animate = function () {
     for (let element of arguments) {
-    element.classList.toggle("active");
+        element.classList.toggle("active");
     }
 }
-  
-  
-function load_page(){
+
+
+function load_page() {
     let name = document.querySelector(".username_bar").innerHTML;
     $.ajax({
         type: "POST",
         url: "/onload_wod",
-        data: { name:name},
-        type: 'POST',
-        success: function(response) {
-        ru_word.value = response.ru;
-        // en_word.value = response.en;
+        data: {
+            name: name
         },
-        error: function(error) {
+        type: 'POST',
+        success: function (response) {
+            ru_word.value = response.ru;
+            // en_word.value = response.en;
+        },
+        error: function (error) {
             console.log(error);
         }
     });
 }
 
 
-Array.prototype.remByVal = function(val) {
+Array.prototype.remByVal = function (val) {
     for (var i = 0; i < this.length; i++) {
         if (this[i] === val) {
             this.splice(i, 1);
@@ -63,7 +66,7 @@ const rand = max => {
     return Math.floor(Math.random() * Math.floor(max));
 }
 // поиск значения по массиву 
-const contains = (arr, elem) =>{
+const contains = (arr, elem) => {
     for (var i = 0; i < arr.length; i++) {
         if (arr[i] === elem) {
             return true;
@@ -71,93 +74,94 @@ const contains = (arr, elem) =>{
     }
     return false;
 }
-// удаление эллемента из массива по значению
-const removeItem = (array, item) =>{
-    for(var i in array){
-        if(array[i]==item){
-            array.splice(i,1);
-            break;
-        }
-    }
-}
+// // удаление эллемента из массива по значению
+// const removeItem = (array, item) => {
+//     for (var i in array) {
+//         if (array[i] == item) {
+//             array.splice(i, 1);
+//             break;
+//         }
+//     }
+// }
 // ````` try`````
 // ````` fuck this function ``````
 
-const RundOfArrays = (arr1,arr2,length_arr_num1) => {
-    
+const RundOfArrays = (arr1, arr2, length_arr_num1) => {
+
     let same_nambers = rand(length_arr_num1);
     // console.log(`выпавшее число ${same_nambers}`);
-    if ((contains(arr1,same_nambers)==true) && (contains(num2,same_nambers)== false)) {
+    if ((contains(arr1, same_nambers) == true) && (contains(num2, same_nambers) == false)) {
         arr1.remByVal(same_nambers);
         arr2.push(same_nambers);
         return same_nambers;
-    } 
-    else if (arr1.length<2) {
+    } else if (arr1.length < 2) {
         same_nambers = arr1[0];
         arr1.remByVal(same_nambers);
         arr2.push(same_nambers);
         return same_nambers;
-    }
-    else {
-        setTimeout(RundOfArrays(arr1,arr2,length_arr_num1),1000);
+    } else {
+        setTimeout(RundOfArrays(arr1, arr2, length_arr_num1), 1000);
     }
 }
 // answer word function
-const take_arrays_to_words = (arrays,length_array_word) =>{
+const take_arrays_to_words = (arrays, length_array_word) => {
     for (let i = 1; i < length_array_word; i++) {
         arrays.push(i);
-      }
+    }
 }
-const first_answer = (num1,num2) => {
-    let ru,en,name;
+const first_answer = (num1, num2) => {
+    let ru, en, name;
     ru = ru_word.value;
     en = en_word.value;
     name = document.querySelector(".username_bar").innerHTML;
     $.ajax({
-      type: "POST",
-      url: "/answer",
-      data: { name:name,ru_word: ru, en_word: en},
-      type: 'POST',
-      success: function(response) {
-        if (response.answer == "true") {
-          counter_answer_true++;
-          correct_answer.innerHTML = counter_answer_true;
-        } else {
-          counter_answer_false++;
-          wrong_answer.innerHTML = counter_answer_false;
-        }
-        if (contrl_to_answer == 0) {
-            take_arrays_to_words(num1,response.length_array_word);
-            contrl_to_answer++;      
-        }
-        const forRand = response.length_array_word;
-        // console.log(num1);
-       
-        if ( num1.length == 1 && (num1.length != num2.length)) {
-                // RundOfArrays(num1,num2,forRand)
-              alert(`${name} ваш словарь закончился!`);
-            
-              showHide_flex(answer_word_btn);
-            //   now_answer_word_btn = document.getElementById("now_answer_word_btn");
-              now_answer_word_btn.style.display = "flex";
-            
-        }
-        else{
-            RundOfArrays(num1,num2,forRand)
+        type: "POST",
+        url: "/answer",
+        data: {
+            name: name,
+            ru_word: ru,
+            en_word: en
+        },
+        type: 'POST',
+        success: function (response) {
+            if (response.answer == "true") {
+                counter_answer_true++;
+                correct_answer.innerHTML = counter_answer_true;
+            } else {
+                counter_answer_false++;
+                wrong_answer.innerHTML = counter_answer_false;
+            }
+            if (contrl_to_answer == 0) {
+                take_arrays_to_words(num1, response.length_array_word);
+                contrl_to_answer++;
+            }
+            const forRand = response.length_array_word;
+            // console.log(num1);
 
-            let next_word = num2[num2.length - 1];
-            next_answer(next_word,name);
+            if (num1.length == 1 && (num1.length != num2.length)) {
+                // RundOfArrays(num1,num2,forRand)
+                alert(`${name} ваш словарь закончился!`);
+
+                showHide_flex(answer_word_btn);
+                //   now_answer_word_btn = document.getElementById("now_answer_word_btn");
+                now_answer_word_btn.style.display = "flex";
+
+            } else {
+                RundOfArrays(num1, num2, forRand)
+
+                let next_word = num2[num2.length - 1];
+                next_answer(next_word, name);
+            }
+        },
+        error: function (error) {
+            console.log(error);
         }
-      },
-      error: function(error) {
-          console.log(error);
-      }
     });
-    
+
 }
 
 
-const next_answer = (next_word,name) => {
+const next_answer = (next_word, name) => {
     // this.next_word = next_word;
     // this.name = name ;
     let name1;
@@ -165,35 +169,41 @@ const next_answer = (next_word,name) => {
     $.ajax({
         type: "POST",
         url: "/next_answer",
-        data: { name:name,id_word: next_word},
-        type: 'POST',
-        success: function(response) {
-        //   console.log(response.next_word)
-          ru_word.value = response.next_word;
+        data: {
+            name: name,
+            id_word: next_word
         },
-        error: function(error) {
+        type: 'POST',
+        success: function (response) {
+            //   console.log(response.next_word)
+            ru_word.value = response.next_word;
+        },
+        error: function (error) {
             console.log(error);
         }
-      });
+    });
 }
 
 
- const now_answer = () => {
+const now_answer = () => {
     $.ajax({
         type: "POST",
         url: "/now_answer",
-        data: { name:name,word: num1[0]},
+        data: {
+            name: name,
+            word: num1[0]
+        },
         type: 'POST',
-        success: function(response) {
-        //   console.log(response.next_word)
-          ru_word.value = response.first_word;//первое слово в массиве
-        //   RundOfArrays(num1,num2,forRand)
-        take_arrays_to_words(num1,response.length_array_word);
+        success: function (response) {
+            //   console.log(response.next_word)
+            ru_word.value = response.first_word; //первое слово в массиве
+            //   RundOfArrays(num1,num2,forRand)
+            take_arrays_to_words(num1, response.length_array_word);
             // let next_word = num2[num2.length - 1];
             // next_answer(next_word,name);
         },
-        error: function(error) {
+        error: function (error) {
             console.log(error);
         }
-      });
- }
+    });
+}
--- static/js/login_js_new.js
+++ static/js/login_js_new.js
@@ -1,27 +1,27 @@
 var
-    come_in      = document.querySelector(".come_in"),//кнопка входа только на нажатие
-    registration = document.querySelector(".registration"),//кнопк регистрации (только на нажатие)
-    come_in_btn  = document.querySelector(".come_in_btn"),//кнопка входа целиком
-    reg_contain  = document.querySelector(".reg_contain"), //поля регистрации  
-    to_reg       = document.querySelector(".to_reg"),//регистрация                                  
+    come_in = document.querySelector(".come_in"), //кнопка входа только на нажатие
+    registration = document.querySelector(".registration"), //кнопк регистрации (только на нажатие)
+    come_in_btn = document.querySelector(".come_in_btn"), //кнопка входа целиком
+    reg_contain = document.querySelector(".reg_contain"), //поля регистрации  
+    to_reg = document.querySelector(".to_reg"), //регистрация                                  
     come_in_contain = document.querySelector(".come_in_contain"),
-    btn_back     = document.querySelector(".btn_back"),// стрелка вправо
-    btn_back_in  = document.querySelector(".btn_back1"),// стрелка вправо
-    wrap         = document.querySelector(".wrap"),//обёртка кнопки
-    wrap1         = document.querySelector(".wrap1"),
-    come_in_btn1  = document.querySelector(".come_in_btn1");
+    btn_back = document.querySelector(".btn_back"), // стрелка вправо
+    btn_back_in = document.querySelector(".btn_back1"), // стрелка вправо
+    wrap = document.querySelector(".wrap"), //обёртка кнопки
+    wrap1 = document.querySelector(".wrap1"),
+    come_in_btn1 = document.querySelector(".come_in_btn1");
 
 
 
-let togleObj1 ={
-    construcrorRegistrationObj(bloks,...typeDisplay){
+let togleObj1 = {
+    construcrorRegistrationObj(bloks, ...typeDisplay) {
         let i = 0;
         for (let block of bloks) {
             block.style.cssText = `display:${typeDisplay[i]};`;
             i++;
         }
     },
-    constructorAnimation(blocksAnimate,...typeAnimate){
+    constructorAnimation(blocksAnimate, ...typeAnimate) {
         let i = 0;
         for (let block of blocksAnimate) {
             block.style.cssText = `animation:${typeAnimate[i]};`;
@@ -32,53 +32,53 @@ let togleObj1 ={
 
 // Переключатели кнопок
 // регистрация первое нажатие
-registration.onclick = function n(){
-    arr = ["flex","none","flex","none","none","flex"];
-    arr1 = [reg_contain,come_in_btn,btn_back,btn_back_in,wrap,wrap1]
-    togleObj1.construcrorRegistrationObj(arr1,...arr);
-} 
+registration.onclick = function n() {
+    arr = ["flex", "none", "flex", "none", "none", "flex"];
+    arr1 = [reg_contain, come_in_btn, btn_back, btn_back_in, wrap, wrap1]
+    togleObj1.construcrorRegistrationObj(arr1, ...arr);
+}
 
 
 // кнопка возврата к выбору
-btn_back.onclick = function (){
-    arr = ["none","block","none","block","none","none"];
-    arr1 = [reg_contain,come_in_btn,btn_back,registration,wrap1,]
-    togleObj1.construcrorRegistrationObj(arr1,...arr);
+btn_back.onclick = function () {
+    arr = ["none", "block", "none", "block", "none", "none"];
+    arr1 = [reg_contain, come_in_btn, btn_back, registration, wrap1, ]
+    togleObj1.construcrorRegistrationObj(arr1, ...arr);
 
 
-    arr1=['form-fly-up 0.8s ease;'];
-    arr=[wrap];
-    togleObj1.constructorAnimation(arr,...arr1);
+    arr1 = ['form-fly-up 0.8s ease;'];
+    arr = [wrap];
+    togleObj1.constructorAnimation(arr, ...arr1);
 }
 
 
 // кнопка первичного входа
-come_in.onclick = function(){
-    arr1=['none;'];
-    arr=[come_in_btn];
-    togleObj1.constructorAnimation(arr,...arr1);
+come_in.onclick = function () {
+    arr1 = ['none;'];
+    arr = [come_in_btn];
+    togleObj1.constructorAnimation(arr, ...arr1);
 
 
-    arr = ["block","none","flex","none","flex"];
-    arr1 = [btn_back_in,to_reg,come_in_contain,come_in_btn,come_in_btn1]
-    togleObj1.construcrorRegistrationObj(arr1,...arr);
+    arr = ["block", "none", "flex", "none", "flex"];
+    arr1 = [btn_back_in, to_reg, come_in_contain, come_in_btn, come_in_btn1]
+    togleObj1.construcrorRegistrationObj(arr1, ...arr);
 }
 
 
 // возврат от первичного входа
-btn_back_in.onclick =function (){
-    arr1=['form-fly-up 0.8s ease;'];
-    arr=[come_in_btn];
-    togleObj1.constructorAnimation(arr,...arr1);
-    
+btn_back_in.onclick = function () {
+    arr1 = ['form-fly-up 0.8s ease;'];
+    arr = [come_in_btn];
+    togleObj1.constructorAnimation(arr, ...arr1);
+
     // come_in_btn.style.cssText = "animation: form-fly-up 0.8s ease;";
-    arr = ["none","none","flex","flex","none"];
-    arr1 = [come_in_contain,btn_back_in,to_reg,come_in_btn,come_in_btn1,]
-    togleObj1.construcrorRegistrationObj(arr1,...arr)
+    arr = ["none", "none", "flex", "flex", "none"];
+    arr1 = [come_in_contain, btn_back_in, to_reg, come_in_btn, come_in_btn1, ]
+    togleObj1.construcrorRegistrationObj(arr1, ...arr)
 }
 // вход и регистрация "logika"
- var
-    reg_name,reg_pas1,reg_pas2,name_com,pas_com;
+var
+    reg_name, reg_pas1, reg_pas2, name_com, pas_com;
 
 
 // *************************************
@@ -89,36 +89,39 @@ wrap1.onclick = function () {
     reg_name = document.getElementById("name_reg").value;
     reg_pas1 = document.getElementById("p1").value;
     reg_pas2 = document.getElementById("p2").value;
-    if (reg_name == "" || reg_pas1 == "" ||  reg_pas2 == "") {
+    if (reg_name == "" || reg_pas1 == "" || reg_pas2 == "") {
         alert("Заполните все поля");
-    }else{
-        if (reg_pas1.length<10) {
+    } else {
+        if (reg_pas1.length < 10) {
             alert("Пароль должени составляять не мениие 10 знаков");
         } else {
             if (reg_pas1 == reg_pas2) {
                 $.ajax({
                     type: "POST",
                     url: "/reg",
-                    data: { reg_name: reg_name, reg_pas: reg_pas1},
+                    data: {
+                        reg_name: reg_name,
+                        reg_pas: reg_pas1
+                    },
                     type: 'POST',
-                    success: function(response) {
+                    success: function (response) {
                         // console.log(response.name);
                         // alert(response.name);
                         if (response.name == "new") {
                             document.location.replace("/index");
-                        }else{
+                        } else {
                             alert(response.name);
                         }
                     },
-                    error: function(error) {
+                    error: function (error) {
                         console.log(error);
                     }
                 });
             } else {
                 alert("Пароли не совподают!");
-            } 
+            }
         }
-        
+
     }
 }
 
@@ -127,25 +130,31 @@ wrap1.onclick = function () {
 // Вход
 //отправкляет имя пользователя и пароль
 // принимает тип имени если 'old' осуществить вход
-come_in_btn1.onclick = function(){
-    name_com = document.getElementById("name_com").value;
-    pas_com = document.getElementById("pas_com").value;
-    if (name_com == "" || pas_com == "") {
+let EnterData = {};
+come_in_btn1.onclick = function () {
+    // name_com = document.getElementById("name_com").value;
+    // pas_com = document.getElementById("pas_com").value;
+    EnterData.name_com = document.getElementById("name_com").value;
+    EnterData.pas_com = document.getElementById("pas_com").value;
+    if (EnterData.name_com == "" || EnterData.pas_com == "") {
         alert("Заполните все поля");
-    }else{
+    } else {
         $.ajax({
             type: "POST",
             url: "/come_in",
-            data: { name_com: name_com, pas_com: pas_com},
+            data: {
+                name_com: EnterData.name_com,
+                pas_com: EnterData.pas_com
+            },
             type: 'POST',
-            success: function(response) {
+            success: function (response) {
                 if (response.name == "old") {
                     document.location.replace("/index");
-                }else{
+                } else {
                     alert("Не верный логин или пароль");
                 }
             },
-            error: function(error) {
+            error: function (error) {
                 console.log(error);
             }
         });
